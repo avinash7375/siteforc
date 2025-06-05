6de0ea4ea5d7f2f7d3e7e950b3286cf2790f157d
Avinash <53809765+avinash7375@users.noreply.github.com>
2025-06-05 21:07:22 +0530

    now

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include <bits/stdc++.h>        //overall cpp standard template library
 
+using namespace std;
+
+int main() {
+
+    //here we are going to create an HPX for stocks prices
+    
+}
